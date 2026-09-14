f9b549a1fd9711a67dac1ee1b5cedc98a62e38d9
divineavriy <amercado@mymail.mapua.edu.ph>
2026-09-14 14:54:39 +0000

    changed notification icon

--- src/App.tsx
+++ src/App.tsx
@@ -815,6 +815,7 @@ function Shell({
             <span>{info.label}</span>
           </div>
           <div className="top-actions">
+<<<<<<< HEAD
             <div className="notification-wrap">
               <button
                 className="notification-button"
@@ -854,6 +855,11 @@ function Shell({
                 </div>
               )}
             </div>
+=======
+            <button className="notification" type="button" aria-label="Notifications">
+              <span aria-hidden="true" />
+            </button>
+>>>>>>> 454ac12 (changed notification icon)
             <span className="online">
               <i /> Campus operations online
             </span>
